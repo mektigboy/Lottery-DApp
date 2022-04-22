8a19ab87e3c83ca1535cc997bcea07bc49536ad2
Ángel Sánchez <mektigboy@protonmail.com>
2022-04-22 19:28:13 +0000

    Update lottery-dapp/src/lottery.js

--- lottery-dapp/src/lottery.js
+++ lottery-dapp/src/lottery.js
@@ -1,58 +1,58 @@
-import web3 from "./web3";
+import web3 from './web3';
 
 // From 'Lottery Smart Contract' project.
-const contractAddress = "0x5F0DDbB79227803276f963b41f8014d1A90988ce";
+const contractAddress = '0x5F0DDbB79227803276f963b41f8014d1A90988ce';
 const abi = [
   {
     constant: true,
     inputs: [],
-    name: "manager",
-    outputs: [{ name: "", type: "address" }],
+    name: 'manager',
+    outputs: [{ name: '', type: 'address' }],
     payable: false,
-    stateMutability: "view",
-    type: "function",
+    stateMutability: 'view',
+    type: 'function',
   },
   {
     constant: false,
     inputs: [],
-    name: "pickWinner",
+    name: 'pickWinner',
     outputs: [],
     payable: false,
-    stateMutability: "nonpayable",
-    type: "function",
+    stateMutability: 'nonpayable',
+    type: 'function',
   },
   {
     constant: true,
     inputs: [],
-    name: "getPlayers",
-    outputs: [{ name: "", type: "address[]" }],
+    name: 'getPlayers',
+    outputs: [{ name: '', type: 'address[]' }],
     payable: false,
-    stateMutability: "view",
-    type: "function",
+    stateMutability: 'view',
+    type: 'function',
   },
   {
     constant: false,
     inputs: [],
-    name: "enter",
+    name: 'enter',
     outputs: [],
     payable: true,
-    stateMutability: "payable",
-    type: "function",
+    stateMutability: 'payable',
+    type: 'function',
   },
   {
     constant: true,
-    inputs: [{ name: "", type: "uint256" }],
-    name: "players",
-    outputs: [{ name: "", type: "address" }],
+    inputs: [{ name: '', type: 'uint256' }],
+    name: 'players',
+    outputs: [{ name: '', type: 'address' }],
     payable: false,
-    stateMutability: "view",
-    type: "function",
+    stateMutability: 'view',
+    type: 'function',
   },
   {
     inputs: [],
     payable: false,
-    stateMutability: "nonpayable",
-    type: "constructor",
+    stateMutability: 'nonpayable',
+    type: 'constructor',
   },
 ];
 
